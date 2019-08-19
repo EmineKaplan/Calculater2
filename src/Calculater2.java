b516ee06875861575e372df57c8611a4b8bd946f
Emine Kaplan <eminekaplantr@gmail.com>
2019-08-18 22:35:04 -0400

    changing

--- src/Calculater2.java
+++ src/Calculater2.java
@@ -1,13 +1,15 @@
 public class Calculater2 {
 
-    public int add( int num1, int num2){
+    public int add(int num1, int num2) {
         return num1 + num2;
     }
 
-    public int subtract(int num1 ,int num2){
-        return num1-num2;
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
 
     }
-    public int multiply(int num1 , int num2){
-        return num1*num2;
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
 }
